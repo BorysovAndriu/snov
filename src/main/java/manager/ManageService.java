9d85a0fbbade73e96b0431976c1e13bd8ed0dd75
fire <andriu.test.post.1@gmail.com>
2020-08-19 08:53:06 +0300

    updated assert methods

--- src/main/java/manager/ManageService.java
+++ src/main/java/manager/ManageService.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
+import static manager.Log.LOG;
+
 public class ManageService {
 
     @Step("Open {url}")
@@ -19,11 +21,17 @@ public class ManageService {
 
     @Step("Verify that condition is true")
     public static void assertTrue(Boolean condition, String errorMessage){
+        if(!condition){
+            LOG.error(errorMessage);
+        }
         Assert.assertTrue(condition);
     }
 
-    @Step("Verify that conditions are equals")
+    @Step("Verify that - {condition1} equals {condition2}")
     public static <T> void assertEquals( T condition1, T condition2, String errorMessage){
+        if(!condition1.equals(condition2)){
+            LOG.error(errorMessage);
+        }
         Assert.assertEquals(condition1, condition2);
     }
 }
